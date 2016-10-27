124bca564e2cc3ab2b9cb5449a9fee9ddbf14e6f
wuuff <wuuuufff@gmail.com>
2016-10-27 07:24:50 -0500

    Fixed bitmap converter, converted spaceship images
    The bitmap converter wasn't yet fully functional, but now it
    will convert from the strange encoding that the fast arduboy
    drawBitmap routine uses.  It may not work on images that are not
    powers of two.  Interestingly, examples of images with unusual
    dimensions in this project all seem to not be encoded unusually
    and therefore may not need to be modified.
    
    Ship images have been converted, but the rest of the images have
    yet to be fixed.  At first I planned to make the script
    automatically parse and fix the image file, but after finding that
    not all the images needed to be converted I doubt it's worth it
    to try to fully automate the conversion process.

--- convert-bitmaps.py
+++ convert-bitmaps.py
@@ -1,9 +1,14 @@
 width = 16
 height = 16
-bitmap = [0x01, 0x01, 0x33, 0xFD, 0x4B, 0xCD, 0x48, 0xFC, 
+bitmap =  [   0x40, 0x80, 0x80, 0x58, 0x68, 0x90, 0x19, 0x26, 
+  0x74, 0x88, 0x90, 0x80, 0x80, 0x80, 0x00, 0x00, 
+  0x00, 0x00, 0x01, 0x02, 0x05, 0x01, 0x01, 0x3E, 
+  0x43, 0x51, 0x40, 0x50, 0x4A, 0x20, 0x1F, 0x00]
+
+'''[0x01, 0x01, 0x33, 0xFD, 0x4B, 0xCD, 0x48, 0xFC, 
   0xB6, 0x01, 0x85, 0x31, 0x86, 0x78, 0x00, 0x00, 
   0x02, 0x02, 0x03, 0x02, 0x03, 0x02, 0x00, 0x00, 
-0x01, 0x02, 0x02, 0x02, 0x01, 0x00, 0x00, 0x00]
+0x01, 0x02, 0x02, 0x02, 0x01, 0x00, 0x00, 0x00]'''
 
 '''[ 0xC0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFE, 0xFF, 0xFF, 
   0xFF, 0xFF, 0xFE, 0xFA, 0xE4, 0xC8, 0x30, 0xC0, 
@@ -35,11 +40,11 @@ for b in bitmap:
     for a in [2**x for x in range(7,-1,-1)]:
         if (a & b) == 0:
             strbuffer+='0'
-            rowbuf[w]+='0'
+            #rowbuf[w]+='0'
             bitbuffer.append(0);
         else:
             strbuffer+='1'
-            rowbuf[w]+='1'
+            #rowbuf[w]+='1'
             bitbuffer.append(1);
         w+=1
         if w == width:
@@ -51,18 +56,63 @@ print 'or...'
 print 'bits:'
 print bitbuffer
 
-rowbuf = ['','','','','','','','','','','','','','','','']
-bytebuf = ['','','','','','','','','','','','','','','','']
+rowbuf = ['' for _ in range(height)]
+bytebuf = ['' for _ in range(height)]
 #Reverse order WHY
 row = height-8
 for i in range(len(bitbuffer)/8):
     for j in range(8):
         rowbuf[row+j]+='%d'%bitbuffer[i*8+j]
-    if i == 15:#i % (width-1) == 0:
+    if (height-i-1)%width == 0:#i % (width-1) == 0:
         row-=8
 
 for r in rowbuf:
-    print r        
+    print r     
+
+    
+def convert_to_bits(bytes):
+    bitbuffer = []
+    for b in bytes:
+        for a in [2**x for x in range(7,-1,-1)]:
+            if (a & b) == 0:
+                bitbuffer.append(0);
+            else:
+                bitbuffer.append(1);
+    return bitbuffer
+    
+def convert_image(bytes, width, height):
+    converted = [[] for _ in range(height)]
+    bitbuffer = convert_to_bits(bytes)
+    print bitbuffer
+    #rowbuf = ['','','','','','','','','','','','','','','','']
+    row = height - 8
+    for i in range(len(bitbuffer)/8):
+        #print converted
+        #print rowbuf
+        for j in range(8):
+            #print 'row %d j %d len %d'%(row,j,len(converted))
+            #print 'adding to row %d value %d'%(row+j, bitbuffer[i*8+j])
+            converted[row+j].append(bitbuffer[i*8+j])
+            #rowbuf[row+j]+='%d'%bitbuffer[i*8+j]
+        if (height-i-1)%width == 0:#i % (width-1) == 0:
+            row-=8
+    return converted
+    
+def string_converted(bits):
+    h = len(bits)
+    w = len(bits[0])/8
+    buf = '%d, %d,\n'%(w,h)
+    for r in reversed(bits): #So either I converted the rows in the wrong order OR it's also stored upside down
+        for c in range(0,len(r)):
+            if c%8 == 0 and c != 0:
+                buf += ',B'
+            elif c == 0:
+                buf += 'B'
+            buf += '%d'%r[c]  
+        buf+=',\n'
+    return buf
+    
+print string_converted(convert_image(bitmap,width,height))
 #for offset in range(16):
 #    strbuffer = ''
     
